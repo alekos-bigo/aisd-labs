b617ded2d0135dfb0057ab50a47e19d101ffd4a2
Aloxiy <96200749+Aloxiy@users.noreply.github.com>
2023-10-05 15:40:20 +0300

    Update QuickS.py

--- QuickS.py
+++ QuickS.py
@@ -1,4 +1,5 @@
 import timeit
+import random
 
 
 def quicksort(arr):
@@ -14,9 +15,10 @@ def quicksort(arr):
 
 
 # Пример использования
-my_list = [3, 6, 8, 10, 1, 2, 1]
-# sorted_list = quicksort(my_list)
-execution_time = timeit.timeit(lambda: quicksort(my_list.copy()), number=1000)
+my_list = [random.randint(1, 10000) for _ in range(10**4)]
+sorted_list = quicksort(my_list)
+print("Отсортированный массив:")
+print(*sorted_list)
+execution_time = timeit.timeit(lambda: quicksort(my_list.copy()), number=100)
 # print(sorted_list)
-print(f"Время выполнения сортировки расчёской: {execution_time} секунд")
-
+print(f"Время выполнения быстрой сортировки: \n{execution_time} секунд")
