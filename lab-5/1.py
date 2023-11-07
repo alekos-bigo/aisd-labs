efae1c49d4d7dfd97911d07d0281c7cbe212b881
Панин Дмитрий Владимирович <dimitri-05@mail.ru>
2023-11-07 18:26:46 +0300

    Update 1.py

--- lab-5/1.py
+++ lab-5/1.py
@@ -1,13 +1,23 @@
 def maximize_stolen_value(N, M, K, weights, values):
-    dp = [[0] * (M + 1) for _ in range(N + 1)]
+    dp = [[0] * (K * M + 1) for _ in range(N + 1)]
 
     for i in range(1, N + 1):
-        for j in range(1, M + 1):
-            dp[i][j] = dp[i - 1][j]
-            if weights[i - 1] <= K:
-                dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + values[i - 1])
+        for j in range(1, K * M + 1):
+            if weights[i - 1] <= j:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weights[i - 1]] + values[i - 1])
+            else:
+                dp[i][j] = dp[i - 1][j]
 
-    return dp[N][M]
+    max_value = dp[N][K * M]
+    stolen_items = []
+    j = K * M
+
+    for i in range(N, 0, -1):
+        if dp[i][j] != dp[i - 1][j]:
+            stolen_items.append(i - 1)
+            j -= weights[i - 1]
+
+    return max_value, stolen_items[::-1]
 
 
 N = 4
@@ -16,5 +26,6 @@ K = 3
 weights = [2, 1, 3, 2]
 values = [12, 10, 20, 15]
 
-max_stolen_value = maximize_stolen_value(N, M, K, weights, values)
-print("Максимальная сумма украденных экспонатов:", max_stolen_value)
+max_value, stolen_items = maximize_stolen_value(N, M, K, weights, values)
+print("Максимальная стоимость украденных экспонатов:", max_value)
+print("Экспонаты, которые вор унес:", *stolen_items)
