3be3038f2f8cb2f0b4e97181516bbd23f440a55a
bananichdev <dimitri-05@mail.ru>
2024-03-19 18:53:41 +0300

    refactor lab6

--- lab6/hashandcrc8.py
+++ lab6/hashandcrc8.py
@@ -1,8 +1,5 @@
 M = int(input("Введите длину массива для значений: "))
-HASH_LEN = 20
 hash_map = {}
-p = 2 ** 64
-m = 11
 
 
 def crc8(data):
@@ -18,36 +15,19 @@ def crc8(data):
     return crc
 
 
-def additive_hash(str_pwd):
-    if len(str_pwd) < m:
-        str_pwd += '*'
-    while len(str_pwd) < m:
-        str_pwd += '0'
-
-    int_pwd = 0
-    # k = 31
-    # l = 0
-    for i in str_pwd:
-        # int_pwd += ord(i) * (k ** l)
-        # l += 1
-        int_pwd += ord(i)
-    return int_pwd % p
-
-
 def div_hash(s_to_hash: str):
-    res = ''
+    res = 0
     for char in s_to_hash:
-        res += str(ord(char) % M)
-    res += "0" * (HASH_LEN - len(res))
-    return int(res)
+        res += ord(char)
+    return int(res) % M
 
 
 try:
     while True:
         s = input("Введите значение: ")
-        hash_map[s] = additive_hash(s)
+        hash_map[s] = div_hash(s)
 except KeyboardInterrupt:
     print()
     print(hash_map)
-    print(crc8(bytes("1", encoding="utf-8")))
+    print(crc8(bytes(s, encoding="utf-8")))
     exit()
