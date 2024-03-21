1ad9d652918c1584d6522c894b1a0010dd4706a6
bananichdev <dimitri-05@mail.ru>
2024-03-21 20:12:22 +0300

    change to naive

--- lab7/2.py
+++ lab7/2.py
@@ -9,33 +9,27 @@ def read_file(filename: str):
     return res
 
 
-def hash_text(words: list, alphabet: list):
-    hashed_words = []
-    alphabet_len = len(alphabet)
-    for idx in range(0, len(words) - 2):
-        hashed_words.append(alphabet.index(words[idx]) * alphabet_len ** 2 +
-                            alphabet.index(words[idx + 1]) * alphabet_len +
-                            alphabet.index(words[idx + 2]))
-    return hashed_words
+def threes(words: list):
+    res = []
+    for idx in range(len(words) - 2):
+        res.append(words[idx] + words[idx + 1] + words[idx + 2])
+    return res
 
 
-def check_plagiarism(original_hashed_words: list, plagiarism_hashed_words: list):
-    if all(word in original_hashed_words for word in plagiarism_hashed_words):
-        return 3 * len(plagiarism_hashed_words)
-    words_c = 0
-    for word in set(original_hashed_words):
-        if word in plagiarism_hashed_words:
-            words_c += 3 * plagiarism_hashed_words.count(word)
-    return words_c
+def check_plagiarism_naive(original_words: list, plagiarism_words: list):
+    set_of_original_threes = set(threes(original_words))
+    list_of_plagiarism_threes = threes(plagiarism_words)
+    c = 0
+    for word in list_of_plagiarism_threes:
+        if word in set_of_original_threes:
+            c += 1
+    return c / len(list_of_plagiarism_threes) * 100
 
 
 def main():
     original_words = read_file("orig.txt")
     plagiarism_words = read_file("plagiarism.txt")
-    alphabet = sorted(set(original_words) | set(plagiarism_words))
-    original_hashed_words = hash_text(original_words, alphabet)
-    plagiarism_hashed_words = hash_text(plagiarism_words, alphabet)
-    print(check_plagiarism(original_hashed_words, plagiarism_hashed_words) / len(plagiarism_words) * 100)
+    print(check_plagiarism_naive(original_words, plagiarism_words))
 
 
 if __name__ == "__main__":
